567284be57dd4dc320d18c53f475f2427cb2fe91
Kamran <balayev.kamran@mail.ru>
2021-02-10 20:00:59 +0300

    Update main.js

--- main.js
+++ main.js
@@ -167,25 +167,7 @@
     }
   });
   
-  
-  
-  /*-----------------------------
-        Speech Synthesis 
-  ------------------------------*/
-  
-  // function readOutLoud(message) {
-  //     var speech = new SpeechSynthesisUtterance();
-  //     speech.lang="tr-TR"
-  //   // Set the text and voice attributes.
-  //     speech.text = message;
-  //     speech.volume = 1;
-  //     speech.rate = 1;
-  //     speech.pitch = 1;
-    
-  //     window.speechSynthesis.speak(speech);
-  // }
-  
-  
+ 
   
   /*-----------------------------
         Helper Functions 
